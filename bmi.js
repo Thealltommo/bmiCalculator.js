d3b0386ab13bd37d59c630d4fba330631281b272
Raymond Thompson <60783171+Thealltommo@users.noreply.github.com>
2022-08-29 11:54:17 +0100

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,9 +1,28 @@
 var weight = prompt("What is your weight in KG?")
 var height = prompt("What is your height in metres?")
 
-function bmiCalculator(weight,height){
+function bmiCalculator (weight, height) {
 
-  var bmi = Math.round(weight / (height * height));
-  return bmi;
+    var interpretation = (weight / Math.pow (height,2));
+
+    if (interpretation <= 18.5) {
+
+        return  "Your BMI is "+interpretation+", so you are underweight." ;
+
+    }
+
+    else if (interpretation > 18.5 && interpretation <= 24.9){
+
+        return  "Your BMI is "+interpretation+", so you have a normal weight." ;
+
+    }
+
+    else if (interpretation > 24.9){
+
+        return  "Your BMI is "+interpretation+", so you are overweight." ;
+
+    }
+
+    return interpretation;
+
 }
-alert("Your bmi is " + bmi);
